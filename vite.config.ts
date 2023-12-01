25d6037e3e47f9ad010cdca1671644fa54b5d9bf
Jordan Ranson <jordan@jordanranson.com>
2023-12-01 14:58:38 -0700

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/advent-of-code-2023/'
+    base: '/advent-of-code-2023/',
+    build: {
+        outDir: './docs'
+    }
 })
